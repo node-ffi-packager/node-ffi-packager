fd36b58395440b4031d37dd41bf8a09d2562d385
Joel Purra <mig@joelpurra.se>
2021-06-09 12:55:21 +0200

    Fail library loading test properly

--- template/generators/app/templates/tests/load-all.js
+++ template/generators/app/templates/tests/load-all.js
@@ -1,24 +1,37 @@
 const { load } = require("../");
 
-async function main() {
-  // Lazy-loading the library and all dependencies.
+async function loadAll() {
+  // NOTE: lazy-loading the library and all dependencies.
   const library = await load();
 
-  // A library might have more than one header file.
+  // NOTE: a library might have more than one header file.
   console.dir(library);
 
-  // Lazy-load all header files.
+  // NOTE: lazy-load all header files.
   const headers = {};
 
   for ([header, headerLoader] of Object.entries(library.headers)) {
     headers[header] = await headerLoader();
   }
 
-  // You can now use the functions and types exported by the header file.
+  // NOTE: you can now use the functions and types exported by the header file.
   console.dir(headers);
 
-  // Properly unload when done.
+  // NOTE: properly unload when done.
   await library.unload();
+}
+
+async function main() {
+  try {
+    await loadAll();
+  } catch (error) {
+    // NOTE: make sure to fail tests.
+    process.exitCode = 1;
+
+    console.error("Library loading test failed.");
+    console.error(__filename);
+    console.error(error);
+  }
 }
 
 main();
